6f941fd6e9719d25626945445a67b6228c9f71d7
Atudoroae Beniamin <atudoroaeben123@gmail.com>
2023-09-11 11:53:27 +0300

    angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div><h1>{{pageTitle}}</h1>
+  <pm-products></pm-products>
+  </div>`
+})
+export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  pageTitle: String = 'ACME Product Manager';
+}

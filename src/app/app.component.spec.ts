a52e62f0e371dfc124fc98fa6ca21e4ca9bb548e
Atudoroae Beniamin <atudoroaeben123@gmail.com>
2023-09-12 12:39:02 +0300

    addded

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app1'`, () => {
+  it(`should have as title 'Nike Store '`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app1');
+    expect(app.title).toEqual('Nike Store');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Nike Store app is running!');
   });
 });

3e09422c05d39e37c27c0499dfa9bbd89b80d705
Atudoroae Beniamin <atudoroaeben123@gmail.com>
2023-09-12 10:01:37 +0300

    added new desing for label

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
+import { IProduct } from "./product";
 
 
 @Component({
     selector:"pm-products",
     templateUrl: "./product-list.component.html",
+    styleUrls: ["./product-list-component.css"],
 
 
 })
@@ -11,15 +13,15 @@ import { Component } from "@angular/core";
 export class ProductListComponent{
   pageTitle: string = "Showe list";
   showImg:boolean = false;
-  listFilter : string = 'cart';
-  imageWidth: number = 50;
-  imageMargin: number = 2;
-  products: any[] = 
+  listFilter : string = '';
+  imageWidth: number = 100;
+  imageHeight:number = 100
+  imageMargin: number = 5;
+  products: IProduct[] = 
   [
 
-    {
-        "productID" : 2,
-        "productName" : "Nike Pegasus 40 ",
+    {   "productID" : 2,
+        "productName" : "Nike Pegasus ",
         "productCode" : "NIK-3697",
         "releaseDate": "2023",
         "description": "Shoe for run",
@@ -29,7 +31,7 @@ export class ProductListComponent{
     },
     {
         "productID" : 2,
-        "productName" : "Nike Vaporfly 3",
+        "productName" : "Nike Vaporfly  ",
         "productCode" : "NIK-4854",
         "releaseDate": "2023",
         "description": "Nike shoe",
@@ -38,8 +40,8 @@ export class ProductListComponent{
         "imageUrl": "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/7be72388-aeb0-4790-8c30-d0793e79cc49/vaporfly-3-eliud-kipchoge-road-racing-shoes-3PPln3.png"
     },
     {
-        "productID" : 2,
-        "productName" : "Nike Structure 25",
+        "productID" : 4,
+        "productName" : "Nike Structure  ",
         "productCode" : "NIK-0908",
         "releaseDate": "2023",
         "description": "Nike shoe",
@@ -48,8 +50,8 @@ export class ProductListComponent{
         "imageUrl": "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/883b91d6-e506-402c-919b-e5f6ae939221/structure-25-road-running-shoes-rVDnPg.png"
     },
     {
-        "productID" : 2,
-        "productName" : "Nike Pegasus 40",
+        "productID" : 5,
+        "productName" : "Nike Pegasus  ",
         "productCode" : "NIK-2549",
         "releaseDate": "2023",
         "description": "Nike shoe",
@@ -58,8 +60,8 @@ export class ProductListComponent{
         "imageUrl": "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/be33804b-50b9-4851-9bc4-1d9b534b9c4f/pegasus-40-road-running-shoes-HTDVsQ.png"
     },
     {
-        "productID" : 2,
-        "productName" : "Nike InfinityRN 4",
+        "productID" : 6,
+        "productName" : "Nike InfinityRN  ",
         "productCode" : "NIK-4560",
         "releaseDate": "2023",
         "description": "Nike shoe",
@@ -68,8 +70,8 @@ export class ProductListComponent{
         "imageUrl": "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/8463e545-701f-496d-a794-3bf38910d604/infinityrn-4-road-running-shoes-Wt3Dww.png"
     },
     {
-        "productID" : 2,
-        "productName" : "Nike Invincible 3",
+        "productID" : 7,
+        "productName" : "Nike Invincible ",
         "productCode" : "NIK-0890",
         "releaseDate": "2023",
         "description": "Nike shoe",
@@ -83,5 +85,8 @@ toggleImage():void{
 
  this.showImg = !this.showImg;
 
+}
+ngOnInit(): void{
+    console.log('In OnInit')
 }
 }
